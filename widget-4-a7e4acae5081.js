63007cb52f169e325ed45d6367ef716ac048feb2
chineseebf <chineseebf@outlook.com>
2024-08-04 19:23:35 +0800

    Add files via upload

---
+++ widget-4-a7e4acae5081.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{141:function(e,n,o){var r=o(560),t=o(19),d=o(532),s=o(585);e.exports=s.extend(d,{renderDecorate:function(){u.call(this,t.getCurrentSound())},onCurrentSoundChange:function(e){u.call(this,e.current)}});function u(e){var n=this.subviews.sound;!e||n&&n.model===e||(n&&n._dispose(),n=this.addSubview(new r({resource_id:e.id,resource_type:"sound"}),"sound"),this.getSoundContainer().append(n.el),n.render())}}}]);
+//# sourceMappingURL=http://ent/web-sourcemaps/widget-4-a7e4acae5081.js.map
